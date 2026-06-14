d814fbd3738fcb47fcc3ae548d8b35d5e9468730
Adarsh Singh <a1d0arsh6@gmail.com>
2026-06-14 13:09:06 +0530

    Quick test script added

---
+++ test.js
@@ -0,0 +1,6 @@
+// PyJS Scratchpad 🚀
+// Write your custom or hidden test JavaScript code here.
+// Run this file using: python main.py test.js (or python3 on macOS/Linux)
+
+let message = "PyJS Interpreter is ready!";
+console.log(message);
